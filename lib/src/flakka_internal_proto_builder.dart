dd8f567add6dc01e6ca6058ef7c79f93ecb32365
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-04-27 10:34:10 -0500

    chore: initial commit

---
+++ lib/src/flakka_internal_proto_builder.dart
@@ -0,0 +1,7 @@
+/// {@template flakka_internal_proto_builder}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class FlakkaInternalProtoBuilder {
+  /// {@macro flakka_internal_proto_builder}
+  const FlakkaInternalProtoBuilder();
+}
---
+++ test/src/flakka_internal_proto_builder_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flakka_internal_proto_builder/flakka_internal_proto_builder.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('FlakkaInternalProtoBuilder', () {
+    test('can be instantiated', () {
+      expect(FlakkaInternalProtoBuilder(), isNotNull);
+    });
+  });
+}
